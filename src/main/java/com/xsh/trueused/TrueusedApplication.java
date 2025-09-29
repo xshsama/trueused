f211d4a034b76463acf15937df698a6fe7a264ba
“xshsama” <2766988086@qq.com>
2025-09-29 18:17:47 +0800

    初始化项目

---
+++ src/main/java/com/xsh/trueused/TrueusedApplication.java
@@ -0,0 +1,13 @@
+package com.xsh.trueused;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrueusedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrueusedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xsh/trueused/TrueusedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsh.trueused;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrueusedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
